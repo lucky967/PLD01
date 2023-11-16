6ecd46872bf73a30c06a83cf5ea7bf36a796ddee
lucky967 <yitnoku@gmail.com>
2023-11-16 00:09:21 +0000

    🟣 auto commit

--- Update.tsx
+++ Update.tsx
@@ -9219,3 +9219,7 @@ curl: (22) The requested URL returned error: 408
                                  Dload  Upload   Total   Spent    Left  Speed
   0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    233      0 --:--:-- --:--:-- --:--:--   234
 {"code":"InvalidParameters","error":"not found","data":null}
+  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
+                                 Dload  Upload   Total   Spent    Left  Speed
+  0     0    0     0    0     0      0      0 --:--:-- --:--:-- --:--:--     0100    60  100    60    0     0    224      0 --:--:-- --:--:-- --:--:--   225
+{"code":"InvalidParameters","error":"not found","data":null}
